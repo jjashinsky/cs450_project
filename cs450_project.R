941fe2159aec9c1725799535ff82a8a2e7ae88a0
libertydynamic1 <isaacwhess@gmail.com>
2019-03-31 00:33:44 +0000

    Updated

--- cs450_project.R
+++ cs450_project.R
@@ -1,38 +1,64 @@
 library(tidyverse)
 
-tournament_data <- read_csv("C:/Users/Owner/Desktop/Isaac School/CS 450/Project/cs450_project/data/NCAATourneyDetailedResults.csv")
-season_data <- read_csv("C:/Users/Owner/Desktop/Isaac School/CS 450/Project/cs450_project/data/RegularSeasonDetailedResults.csv")
+game_data <- read_csv("derived_data/game_data_full.csv")
 
-unique(season_data$WTeamID)
-unique(tournament_data$WTeamID)
 
-full_data <- bind_rows(season_data, tournament_data)
+game_data_2 <- game_data %>% 
+  mutate(team1_score = `score.x`,
+         team1_num_ot = `num_ot.x`,
+         team1_fgm = `fgm.x`,
+         team1_fga = `fga.x`,
+         team1_fgm3 = `fgm3.x`,
+         team1_fga3 = `fga3.x`,
+         team1_ftm = `ftm.x`,
+         team1_fta = `fta.x`,
+         team1_or = `or.x`,
+         team1_dr = `dr.x`,
+         team1_ast = `ast.x`,
+         team1_to = `to.x`,
+         team1_stl = `stl.x`,
+         team1_blk = `blk.x`,
+         team1_pf = `pf.x`,
+         team1_won_previous_game = `won_previous_game.x`,
+         team2_score = `score.y`,
+         team2_num_ot = `num_ot.y`,
+         team2_fgm = `fgm.y`,
+         team2_fga = `fga.y`,
+         team2_fgm3 = `fgm3.y`,
+         team2_fga3 = `fga3.y`,
+         team2_ftm = `ftm.y`,
+         team2_fta = `fta.y`,
+         team2_or = `or.y`,
+         team2_dr = `dr.y`,
+         team2_ast = `ast.y`,
+         team2_to = `to.y`,
+         team2_stl = `stl.y`,
+         team2_blk = `blk.y`,
+         team2_pf = `pf.y`,
+         team2_won_previous_game = `won_previous_game.y`) %>% 
+  select(team1_win, team1_id, team2_id, team1_game_order, team2_game_order, team1_score, team1_num_ot, team1_fgm, team1_fga, team1_fgm3, team1_fga3, team1_ftm, team1_fta, team1_or, team1_dr, team1_ast, team1_to, team1_stl, team1_blk, team1_pf, team1_won_previous_game, team2_score, team2_num_ot, team2_fgm, team2_fga, team2_fgm3, team2_fga3, team2_ftm, team2_fta, team2_or, team2_dr, team2_ast, team2_to, team2_stl, team2_blk, team2_pf, team2_won_previous_game)
 
-full_data1 <- full_data %>% 
-  select(Season, DayNum, WTeamID, WScore, WLoc, NumOT, WFGM, WFGA, WFGM3, WFGA3, WFTM, WFTA, WOR, WDR, WAst, WTO, WStl, WBlk, WPF)
+game_data_3 <- game_data_2 %>% 
+  mutate(diff_game_order = team1_game_order - team2_game_order,
+         diff_score = team1_score - team2_score,
+         diff_num_ot = team1_num_ot - team2_num_ot,
+         diff_fgm = team1_fgm - team2_fgm,
+         diff_fga = team1_fga - team2_fga,
+         diff_fgm3 = team1_fgm3 - team2_fgm3,
+         diff_fga3 = team1_fga3 - team2_fga3,
+         diff_ftm = team1_ftm - team2_ftm,
+         diff_fta = team1_fta - team2_fta,
+         diff_or = team1_or - team2_or,
+         diff_dr = team1_dr - team2_dr,
+         diff_ast = team1_ast - team2_ast,
+         diff_to = team1_to - team2_to,
+         diff_stl = team1_stl - team2_stl,
+         diff_blk = team1_blk - team2_blk,
+         diff_pf = team1_pf - team2_pf,
+         diff_won_previous_game = team1_won_previous_game - team2_won_previous_game) %>% 
+  select(team1_win, team1_id, team2_id, diff_game_order, diff_score, diff_num_ot, diff_fgm, diff_fga, diff_fgm3, diff_fga3, diff_ftm, diff_fta, diff_or, diff_dr, diff_ast, diff_to, diff_stl, diff_blk, diff_pf, diff_won_previous_game)
+  
 
-full_data2 <- full_data %>% 
-  select(Season, DayNum, LTeamID, LScore, WLoc, NumOT, LFGM, LFGA, LFGM3, LFGA3, LFTM, LFTA, LOR, LDR, LAst, LTO, LStl, LBlk, LPF)
-
-full_data1 <- full_data1 %>% 
-  mutate(TeamID = WTeamID, Score = WScore, Loc = WLoc, FGM = WFGM, FGA = WFGA, FGM3 = WFGM3, FGA3 = WFGA3, FTM = WFTM, FTA = WFTA, OR = WOR, DR = WDR, Ast = WAst, TO = WTO, Stl = WStl, Blk = WBlk, PF = WPF) %>% 
-  select(-WTeamID, -WScore, -WLoc, -WFGM, -WFGA, -WFGM3, -WFGA3, -WFTM, -WFTA, -WOR, -WDR, -WAst, -WTO, -WStl, -WBlk, -WPF)
-
-full_data2 <- full_data2 %>% 
-  mutate(TeamID = LTeamID, Score = LScore, Loc = WLoc, FGM = LFGM, FGA = LFGA, FGM3 = LFGM3, FGA3 = LFGA3, FTM = LFTM, FTA = LFTA, OR = LOR, DR = LDR, Ast = LAst, TO = LTO, Stl = LStl, Blk = LBlk, PF = LPF) %>% 
-  select(-LTeamID, -LScore, -WLoc, -LFGM, -LFGA, -LFGM3, -LFGA3, -LFTM, -LFTA, -LOR, -LDR, -LAst, -LTO, -LStl, -LBlk, -LPF)
-
-new_data <- bind_rows(full_data1, full_data2)
-
-new_data1 <- new_data %>% 
-  group_by(TeamID) %>% 
-  arrange(Season, DayNum, .by_group = TRUE) %>% 
-  mutate(team_game_order = row_number()) %>% 
-  ungroup()
-
-new_data2 <- new_data1 %>% 
-  mutate(key = str_c(Season, DayNum, TeamID)) %>% 
-  select(key, team_game_order)
-
+write_csv(game_data_3, "derived_data/game_data_ml.csv")
 
 
---
+++ cs450_project.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 02:19:15 2019
+
+@author: Isaac
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 00:55:54 2019
+
+@author: Isaac
+"""
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+import copy
+import pydotplus
+from sklearn import tree
+from collections import Counter
+from sklearn.neural_network import MLPClassifier 
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+
+
+
+
+
+
+
+def getProjectData():
+    project_data = pd.read_csv('C:/Users/Owner/Desktop/Isaac School/CS 450/Project/cs450_project/derived_data/game_data_ml.csv')
+    return project_data
+
+
+
+def dataTargetSplit(data, target_column_name):
+    X = data.drop(columns=[target_column_name]).values
+    y = data[target_column_name].values.flatten()
+    return (X, y)
+
+def split_data(data, target_column_name):
+    X, y = dataTargetSplit(data, target_column_name)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    return (X_train, X_test, y_train, y_test)
+
+def split_data_x_y(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    return (X_train, X_test, y_train, y_test)
+
+def evaluate_predictions(targets_predicted, answers, show_values, name_classifier = ""):
+    print(name_classifier)
+    predictions = copy.deepcopy(targets_predicted)
+    i = 0
+    for predicted in targets_predicted:
+        if predicted == answers[i]:
+            predictions[i] = 1
+        else:
+            predictions[i] = 0
+        i += 1
+    if show_values == True:
+        print(predictions)
+    correct = sum(predictions)
+    data_set_value = Counter(targets_predicted).keys()
+    data_set_values = []
+    for num in data_set_value:
+        data_set_values.append(num)
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+    positive = 0
+    negative = 0
+    for i in range(len(answers)):
+        added = True
+        for v in data_set_values:
+            if added:
+                if targets_predicted[i]==answers[i]==data_set_values[data_set_values.index(v)]:
+                    positive += 1
+                    TP += 1
+                    added = False
+                    break
+                if answers[i]==data_set_values[data_set_values.index(v)] and targets_predicted[i]!=answers[i]:
+                    negative += 1
+                    FP += 1
+                    added = False
+                    break
+                if targets_predicted[i]==answers[i]!=data_set_values[data_set_values.index(v)]:
+                    negative += 1
+                    TN += 1
+                    added = False
+                    break
+                if answers[i]!=data_set_values[data_set_values.index(v)] and targets_predicted[i]!=answers[i]:
+                    positive += 1
+                    FN += 1
+                    added = False
+                    break
+    if positive == 0:
+        positive = 1
+    if negative == 0:
+        negative = 1
+    print("Single Run Prediction Accuracy: {:.1f}%" .format((correct/len(predictions))*100))
+    print("True Positive rate: {:.1f}%" .format((TP/positive)*100))
+    print("False Positive rate: {:.1f}%" .format((FP/negative)*100))
+    print("True Negative rate: {:.1f}%" .format((TN/negative)*100))
+    print("False Negative rate: {:.1f}%" .format((FN/positive)*100))
+    print("True Positive count: {}" .format(TP))
+    print("False Positive count: {}" .format(FP))
+    print("True Negative count: {}" .format(TN))
+    print("False Negative count: {}" .format(FN))
+    print("Total: {}" .format(i))
+    data_values = Counter(targets_predicted).values()
+    print(data_values)
+    print("")
+    
+    
+    
+    
+
+def standardizeData(scaling_data, data):
+    std_scale = preprocessing.StandardScaler().fit(scaling_data)
+    X_train_std = std_scale.transform(data)
+    train_std = X_train_std.tolist()
+    return train_std
+
+
+
+def main():
+    project_data = getProjectData()
+    
+    X_data, y_data = dataTargetSplit(project_data, "team1_win")
+    X_train, X_test, y_train, y_test = split_data(project_data, "team1_win")
+
+#    X_train_std = standardizeData(X_train, X_train)
+#    X_test_std = standardizeData(X_train, X_test)
+
+
+
+
+    #Decision Tree Classifier
+    decisionTreeClassifier = DecisionTreeClassifier(criterion='entropy', random_state=0, max_depth=5)
+    
+    scores = cross_val_score(decisionTreeClassifier, X_data, y_data, cv=5)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    decisionTreeClassifier.fit(X_train, y_train)
+    predictions = decisionTreeClassifier.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Decision Tree")
+    #Make a graph of the tree
+    dot_data = tree.export_graphviz(decisionTreeClassifier, out_file=None, 
+                                    feature_names=list(project_data.drop(columns=["team1_win"])),  
+                                    class_names="team1_win")
+    graph = pydotplus.graph_from_dot_data(dot_data)  
+    graph.write_png("C:/Users/Owner/Desktop/Isaac School/CS 450/Project/cs450_project/graphs/decision_tree.png")
+
+
+
+
+    #Neural Network Classifer
+    neural_network = MLPClassifier(hidden_layer_sizes=(40, ), activation='logistic', solver='adam', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=True, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10)
+    
+    scores = cross_val_score(neural_network, X_data, y_data, cv=10)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    neural_network.fit(X_train, y_train)
+    predictions = neural_network.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Neural Network")
+
+
+
+
+    #Bagging Classifer with KNeighbors
+    bagging = BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5)    
+
+    scores = cross_val_score(bagging, X_data, y_data, cv=50)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    bagging.fit(X_train, y_train)
+    predictions = bagging.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Bagging Classifier")
+    
+    
+
+    
+    #Ada Boost Classifier
+    ada_boost_classifier = AdaBoostClassifier(n_estimators=100)
+
+    scores = cross_val_score(ada_boost_classifier, X_data, y_data, cv=10)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    ada_boost_classifier.fit(X_train, y_train)
+    predictions = ada_boost_classifier.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Ada Boost Classifier")
+
+
+
+    #Gradient Boosting Classifier
+    gradient_boosting_classifier = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto', validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)
+
+    scores = cross_val_score(gradient_boosting_classifier, X_data, y_data, cv=10)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    gradient_boosting_classifier.fit(X_train, y_train)
+    predictions = gradient_boosting_classifier.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Gradient Boosting Classifier")
+
+
+
+  
+    #Random Forest Classifier
+    random_forest_classifier = RandomForestClassifier(n_estimators=200, n_jobs=-1, oob_score=True, max_depth=5 )
+
+    scores = cross_val_score(random_forest_classifier, X_data, y_data, cv=10)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    random_forest_classifier.fit(X_train, y_train)
+    predictions = random_forest_classifier.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Random Forest Classifier")
+
+    # Extract single tree and write to png
+    estimator = random_forest_classifier.estimators_[6]
+    dot_data = tree.export_graphviz(estimator, out_file=None, 
+                                    feature_names=list(project_data.drop(columns=["team1_win"])),  
+                                    class_names="team1_win")
+    graph = pydotplus.graph_from_dot_data(dot_data)  
+    graph.write_png("C:/Users/Owner/Desktop/Isaac School/CS 450/Project/cs450_project/graphs/random_forest_decision_tree.png")
+
+    
+    
+    #Support Vector Machine Classifier
+    support_vector_machine_classifier = svm.SVC(C=1.0, kernel='rbf', degree=3, gamma='scale', coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape='ovr', random_state=None)
+
+    scores = cross_val_score(support_vector_machine_classifier, X_data, y_data, cv=10)
+    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    support_vector_machine_classifier.fit(X_train, y_train)
+    predictions = support_vector_machine_classifier.predict(X_test)
+    evaluate_predictions(predictions, y_test, False, "Support Vector Machine Classifier")
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
+
